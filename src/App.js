d5cf7667c5d7e93e60bbf2b9141a7416b1e04dba
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-08-01 21:39:35 +0700

    implements add book

--- src/App.js
+++ src/App.js
@@ -13,8 +13,11 @@ const App = () => {
     new Book('Functional Programming', 'author', 100, true),
     new Book('Functional Programming', 'author', 100, true)
   ]
+  const defaultInputBook = { title: '', author: '', pages: '', finish: false }
+
   const [bookshelf, setBookshelf] = useState(dummies)
   const [willAdd, setWillAdd] = useState(false)
+  const [inputBook, setInputBook] = useState(defaultInputBook)
 
   function Book(title, author, pages, finish) {
     this.id = Math.random() * 10 ** 18
@@ -24,15 +27,24 @@ const App = () => {
     this.finish = finish
   }
 
-  const addBookToBookshelf = book => {
+  const addBookToBookshelf = () => {
+    const book = new Book(...Object.values(inputBook))
     setBookshelf(bookshelf.concat(book))
+    setWillAdd(false)
+    setInputBook(defaultInputBook)
   }
 
   const addBook = () => setWillAdd(!willAdd)
 
   return (
     <div className='App'>
-      <AddBook willAdd={willAdd} addBook={addBook} />
+      <AddBook
+        willAdd={willAdd}
+        addBook={addBook}
+        addBookToBookshelf={addBookToBookshelf}
+        inputBook={inputBook}
+        setInputBook={setInputBook}
+      />
       <Navigation />
       <Bookshelf bookshelf={bookshelf} addBook={addBook} />
     </div>
--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,20 @@
 import './AddBook.css'
 import './coolcheckbox.css'
 
-const AddBook = ({ willAdd, addBook }) => {
+const AddBook = ({ willAdd, addBook, addBookToBookshelf, inputBook, setInputBook }) => {
+  const submitBook = event => {
+    event.preventDefault()
+    addBookToBookshelf()
+  }
+
+  const handleOnChange = toChange => event => {
+    const newInputBook = { ...inputBook }
+    newInputBook[toChange] = event.target.type === 'checkbox'
+      ? event.target.checked
+      : event.target.value
+    setInputBook(newInputBook)
+  }
+
   if (willAdd) {
     return (
       <>
@@ -9,14 +22,21 @@ const AddBook = ({ willAdd, addBook }) => {
         <div className='add-book-form flex-down'>
           <form className='flex-down'>
             <p>Add new book</p>
-            <input placeholder='Title'></input>
-            <input placeholder='Author'></input>
-            <input placeholder='Pages'></input>
+            <input value={inputBook.title} onChange={handleOnChange('title')} placeholder='Title' />
+            <input value={inputBook.author} onChange={handleOnChange('author')} placeholder='Author' />
+            <input value={inputBook.pages} onChange={handleOnChange('pages')} placeholder='Pages'
+              type='number'
+            />
             <div className='flex-right checkbox-wrapper-2'>
-              <label for='read'>Have you read it?</label>
-              <input type='checkbox' id='read' className='sc-gJwTLC ikxBAC'></input>
+              <label htmlFor='read'>Have you read it?</label>
+              <input
+                type='checkbox'
+                id='read'
+                className='sc-gJwTLC ikxBAC'
+                onChange={handleOnChange('finish')}
+              />
             </div>
-            <input type='submit' value='Submit' className='btn'></input>
+            <input type='submit' value='Submit' className='btn' onClick={submitBook}></input>
           </form>
         </div>
       </>
