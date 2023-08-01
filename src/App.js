6e27712a361c626a56e219626e81d93c6c3da01d
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-08-01 14:12:47 +0700

    display book card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import './Navigation.css'
+import './Bookshelf.css'
 
-function App() {
+const App = () => {
+  const dummies = [
+    new Book('Functional Programming', 'author', 100, true),
+    new Book('Functional Programming', 'author', 100, true),
+    new Book('Functional Programming', 'author', 100, true)
+  ]
+  const [bookshelf, setBookshelf] = useState(dummies)
+
+  function Book(title, author, pages, finish) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.finish = finish
+  }
+
+  const addBookToBookshelf = book => {
+    setBookshelf(bookshelf.concat(book))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navigation />
+      <Bookshelf bookshelf={bookshelf} />
     </div>
-  );
+  )
 }
 
-export default App;
+const Navigation = () => {
+  return (
+    <div className='Navigation'>
+      <div className='title'>My Library</div>
+    </div>
+  )
+}
+
+const Bookshelf = ({ bookshelf }) => {
+  const createCard = book => {
+    return (
+      <div key={book.title + book.author} className='card'>
+        <p>{book.title}</p>
+        <p>{book.author}</p>
+        <p>{book.pages} pages</p>
+        <p>{String(book.finish)}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className='Bookshelf'>
+      {bookshelf.map(book => createCard(book))}
+    </div>
+  )
+}
+
+export default App
